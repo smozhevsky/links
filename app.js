79ac26d3098049740cbeec83e27d321574d6f1ea
smozhevsky <smozhevsky@gmail.com>
2021-02-04 14:01:38 +0300

    add contacts button

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 let menuButton = document.querySelector(".menu-button");
 let nav = document.querySelector(".nav");
+let aboutButton = document.querySelector(".about");
+let userLinks = document.querySelector(".user-links");
+let contactsButton = document.querySelector(".contacts");
 
 menuButton.addEventListener("click", function () {
   menuButton.classList.toggle("active");
   nav.classList.toggle("active");
+});
+
+contactsButton.addEventListener("click", () => {
+  userLinks.style.display = 'block'
 });
