4db054094347ad8f8f98c867e62fcabf0290dbbe
smozhevsky <smozhevsky@gmail.com>
2021-02-05 09:46:39 +0300

    add about

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 let menuButton = document.querySelector(".menu-button");
 let nav = document.querySelector(".nav");
-let aboutButton = document.querySelector(".about");
+let aboutButton = document.querySelector(".about-btn");
 let userLinks = document.querySelector(".user-links");
 let contactsButton = document.querySelector(".contacts");
+let headerLogo = document.querySelector("header-logo");
+let aboutMe = document.querySelector(".about")
 
 menuButton.addEventListener("click", function () {
   menuButton.classList.toggle("active");
@@ -11,4 +13,8 @@ menuButton.addEventListener("click", function () {
 
 contactsButton.addEventListener("click", () => {
   userLinks.classList.toggle("active");
+});
+
+aboutButton.addEventListener("click", () => {
+  aboutMe.classList.toggle("active");
 });
